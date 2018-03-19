e8faec08f46bdc389792198d8da3413bf80592e7
Hanwen Zhao <hazh1387@colorado.edu>
2018-03-19 10:13:56 -0600

    update launch file

--- robotics/scripts/pololuControl.py
+++ robotics/scripts/pololuControl.py
@@ -30,12 +30,16 @@ def callback(data):
     print "Servo: " + str(CENTER_VALUE-int((data.data)))
     #time.sleep(1)
 
-def listener():
-    setup()
-    rospy.init_node('servoControl_node', anonymous = True)
-    rospy.Subscriber("control_effort", Float64, callback)
-    rospy.spin()
 
 if __name__ == '__main__':
     setup()
-    listener()
+    while not rospy.is_shutdown():
+        rospy.init_node('pololuControl_node', anonymous = True)
+        rospy.Subscriber("control_effort", Float64, callback)
+        pub = rospy.Publisher('state', Float64, queue_size=10)
+        ir_output = int((servo.getPosition(6)/4+245)*16-17)
+        if abs(5648 - ir_output) > 100:
+            pub.publish(ir_output)
+        else:
+            pub.publish(5648)
+        time.sleep(1/50)
