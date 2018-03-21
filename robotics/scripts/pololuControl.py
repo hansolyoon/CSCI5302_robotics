e491414b8388543dc0a590db6f6471d0ccc9b0e7
Hanwen Zhao <hazh1387@colorado.edu>
2018-03-21 14:47:10 -0600

    Auto stash before merge of "singleIR" and "origin/singleIR"
    Working code for corners(not for bays)

--- robotics/scripts/pololuControl.py
+++ robotics/scripts/pololuControl.py
@@ -5,14 +5,16 @@ import time
 from std_msgs.msg import Float64
 from std_msgs.msg import Bool
 
+# initilize the pololu control
 servo = maestro.Controller()
+# define center and range value for servo
 CENTER_VALUE = 1500 * 4
 EXTREME_LEFT = 4100
 EXTREME_RIGHT = 7900
-
+# initilize two publishers
 pub = rospy.Publisher('state', Float64, queue_size=10)
 pub2 = rospy.Publisher('pid_enable', Bool, queue_size=10)
-
+# setup functio
 def setup():
     servo.setSpeed(0, 0)
     servo.setAccel(0, 0)
@@ -21,37 +23,37 @@ def setup():
     servo.setRange(0, EXTREME_LEFT, EXTREME_RIGHT)
     servo.setTarget(0, CENTER_VALUE)
     servo.setRange(1, EXTREME_LEFT, EXTREME_RIGHT)
-    #servo.setTarget(1, 6000)
     time.sleep(1)
+    # set servo speed
     servo.setTarget(1, 6350)
     #servo.setTarget(1, 6170)
     cmd_servo = CENTER_VALUE
+    # initilize the ROS node
     rospy.init_node('pololuControl_node', anonymous = True)
+    # initilize the Subscriber for PID output
     rospy.Subscriber("control_effort", Float64, callback)
 
 def callback(data):
     #rospy.loginfo(rospy.get_caller_id() + " Data to servo %f", data.data)
-    servo.setTarget(0, CENTER_VALUE+int((data.data)))
+    # operate the servo
+    servo.setTarget(0, CENTER_VALUE-int((data.data)))
     #print "Servo: " + str(CENTER_VALUE+int((data.data)))
     #time.sleep(1)
 
 def worker():
+    # set frequnce
     rate = rospy.Rate(200)
     while not rospy.is_shutdown():
+        # make sure PID is enabled all the time
         pub2.publish(True)
         #ir_output = int((servo.getPosition(6)/4+245)*16-17)
+        # inverse the measurement
         ir_output = int(1.0/servo.getPosition(6)*10e5)
         print "IR: " + str(ir_output)
-        if abs(1923 - ir_output) > 50 :
+        if abs(ir_output - 1923) > 10:
             pub.publish(ir_output)
         else:
-            pub.publish(1923)
-
-        if 2900 < ir_output < 3600
-            pub.publish(1850)
-        else:
-            pub.publish(ir_output)
-
+            pub.publish(1915)
         rate.sleep()
     if rospy.is_shutdown():
         servo.setTarget(1, 6000)
