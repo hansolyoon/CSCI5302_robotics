def478153be2a1d5ecd4ce753a280998a07f6edb
Hanwen Zhao <hazh1387@colorado.edu>
2018-04-30 19:12:29 -0600

    initial dfift commit

--- libuvc_ros/libuvc_camera/setup_custom_pythonpath.sh
+++
@@ -1,4 +0,0 @@
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/odroid/catkin_ws/src/libuvc_ros/libuvc_camera/devel/lib/python2.7/dist-packages:$PYTHONPATH
-exec "$@"
--- robotics/scripts/pololuControl.py
+++ robotics/scripts/pololuControl.py
@@ -4,16 +4,21 @@ import maestro
 import time
 from std_msgs.msg import Float64
 from std_msgs.msg import Bool
+from sensor_msgs.msg import Imu
 
 # initilize the pololu control
 servo = maestro.Controller()
 # define center and range value for servo
-CENTER_VALUE = 1500 * 4
+CENTER_VALUE = 6500
 EXTREME_LEFT = 4100
 EXTREME_RIGHT = 7900
 # initilize two publishers
 pub = rospy.Publisher('state', Float64, queue_size=10)
 pub2 = rospy.Publisher('pid_enable', Bool, queue_size=10)
+global controlEffort_IR
+global controlEffort_IMU
+controlEffort_IR = 0
+controlEffort_IMU = 0
 # setup functio
 def setup():
     servo.setSpeed(0, 0)
@@ -21,23 +26,33 @@ def setup():
     servo.setSpeed(1, 0)
     servo.setAccel(1, 0)
     servo.setRange(0, EXTREME_LEFT, EXTREME_RIGHT)
+    servo.setRange(1, 4000, 8000)
     servo.setTarget(0, CENTER_VALUE)
-    servo.setRange(1, EXTREME_LEFT, EXTREME_RIGHT)
-    time.sleep(1)
+    time.sleep(3)
 
     # set servo speed
-    servo.setTarget(1, 6190)
+    servo.setTarget(1, 6500)
     #servo.setTarget(1, 6170)
     cmd_servo = CENTER_VALUE
     # initilize the ROS node
     rospy.init_node('pololuControl_node', anonymous = True)
     # initilize the Subscriber for PID output
     rospy.Subscriber("control_effort", Float64, callback)
+    rospy.Subscriber("imu/data", Imu, callback2)
+
+def callback2(data):
+    #print data.angular_velocity.z
+    global controlEffort_IMU
+    controlEffort_IMU = -int(data.angular_velocity.z*400)
+
 
 def callback(data):
     #rospy.loginfo(rospy.get_caller_id() + " Data to servo %f", data.data)
     # operate the servo
-    servo.setTarget(0, CENTER_VALUE+int((data.data)))
+    #servo.setTarget(0, 6000)
+    #servo.setTarget(0, CENTER_VALUE+int((data.data)))
+    global controlEffort_IR
+    controlEffort_IR = int(data.data)
     #print "Servo: " + str(int((data.data)))
     #time.sleep(1)
 
@@ -50,13 +65,17 @@ def worker():
         #ir_output = int((servo.getPosition(6)/4+245)*16-17)
         # inverse the measurement
         ir_output_left = int(1.0/servo.getPosition(6)*10e5)
-        ir_output_right = int(1.0/servo.getPosition(11)*10e5)+990
+        ir_output_right = int(1.0/servo.getPosition(11)*10e5)-500
         ir_output_diff = ir_output_left - ir_output_right
-        print "IR: " + str(ir_output_diff)
         pub.publish(ir_output_diff)
+        #print "IR: " + str(ir_output_diff)
+        print "IR_Control " + str(controlEffort_IR)
+        print "IMU_Control " + str(controlEffort_IMU)
+        servo.setTarget(0, CENTER_VALUE+controlEffort_IR+controlEffort_IMU)
         rate.sleep()
     if rospy.is_shutdown():
-        servo.setTarget(1, 6000)
+        servo.setTarget(1, 3000)
+        servo.setTarget(0, CENTER_VALUE)
 
 if __name__ == '__main__':
     try:
